3fa03622eb78f75ceaac68ac462337f104d65874
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-07-11 19:01:37 -0600

    mobile fully done, and the menu

--- SHL/src/App.tsx
+++ SHL/src/App.tsx
@@ -6,14 +6,14 @@ import Partner from "./pages/Partner";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="events" element={<Events />} />
-      <Route path="partner" element={<Partner />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route index element={<Home />} />
+			<Route path="events" element={<Events />} />
+			<Route path="partner" element={<Partner />} />
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+	);
 }
 
 export default App;
--- SHL/src/components/TopNav.jsx
+++ SHL/src/components/TopNav.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const TopNav = () => {
-  return (
-    <div className="navbarContainer">
-      <h1 className="title">Star Hack Alliance</h1>
-      <div className="navbar-item-container">
-        <a href="./" className="no-underline">
-          <p className="navbar-items">Home</p>
-        </a>
-        <a href="./partner" className="no-underline">
-          <p className="navbar-items">Partner</p>
-        </a>
-        <a href="./events" className="no-underline">
-          <p className="navbar-items">Events</p>
-        </a>
-      </div>
-    </div>
-  );
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	};
+
+	return (
+		<div className="navbarContainer">
+			<h1 className="title">Star Hack Alliance</h1>
+			<div className="navbar-item-container">
+				<a href="./" className="no-underline">
+					<p className="navbar-items">Home</p>
+				</a>
+				<a href="./partner" className="no-underline">
+					<p className="navbar-items">Partner</p>
+				</a>
+				<a href="./events" className="no-underline">
+					<p className="navbar-items">Events</p>
+				</a>
+			</div>
+			<button className="hamburger" onClick={toggleMenu}>
+				☰
+			</button>
+			{menuOpen && (
+				<div className="mobile-menu">
+					<a href="./" className="no-underline">
+						Home
+					</a>
+					<a href="./partner" className="no-underline">
+						Partner
+					</a>
+					<a href="./events" className="no-underline">
+						Events
+					</a>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default TopNav;

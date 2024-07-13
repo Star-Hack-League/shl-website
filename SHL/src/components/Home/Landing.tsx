4dded24a75623a76cd6a40d38fe6d2cfb7e3beb7
DinosaurPotato534 <shaany0418@gmail.com>
2024-07-13 00:56:27 -0400

    fix

--- SHL/src/components/Home/Landing.tsx
+++ SHL/src/components/Home/Landing.tsx
@@ -60,17 +60,6 @@ function Landing() {
 
 				<div className="navbarContainer">
 					<h1 className="title">Star Hack Alliance</h1>
-					<div className="navbar-item-container">
-						<a href="./" className="no-underline">
-							<p className="navbar-items">Home</p>
-						</a>
-						<a href="./partner" className="no-underline">
-							<p className="navbar-items">Partner</p>
-						</a>
-						<a href="./events" className="no-underline">
-							<p className="navbar-items">Events</p>
-						</a>
-					</div>
 					<button className="hamburger" onClick={toggleMenu}>
 						☰
 					</button>
@@ -88,12 +77,6 @@ function Landing() {
 							<div className="mobile-links-wrapper">
 								<a href="./" className="mobile-link">
 									Home
-								</a>
-								<a href="./partner" className="mobile-link">
-									Partner
-								</a>
-								<a href="./events" className="mobile-link">
-									Events
 								</a>
 							</div>
 						</div>
--- SHL/src/components/TopNav.jsx
+++ SHL/src/components/TopNav.jsx
@@ -10,17 +10,6 @@ const TopNav = () => {
 	return (
 		<div className="navbarContainer">
 			<h1 className="title">Star Hack Alliance</h1>
-			<div className="navbar-item-container">
-				<a href="./" className="no-underline">
-					<p className="navbar-items">Home</p>
-				</a>
-				<a href="./partner" className="no-underline">
-					<p className="navbar-items">Partner</p>
-				</a>
-				<a href="./events" className="no-underline">
-					<p className="navbar-items">Events</p>
-				</a>
-			</div>
 			<button className="hamburger" onClick={toggleMenu}>
 				☰
 			</button>
@@ -28,12 +17,6 @@ const TopNav = () => {
 				<div className="mobile-menu">
 					<a href="./" className="no-underline">
 						Home
-					</a>
-					<a href="./partner" className="no-underline">
-						Partner
-					</a>
-					<a href="./events" className="no-underline">
-						Events
 					</a>
 				</div>
 			)}
